93d8fca4a163684886515c3e4f5b61403080da42
juan castañeda <juanemporis123>
2022-07-30 15:47:48 -0500

    Initial Commit

---
+++ App Clima/App_ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ClimaApp.swift
+//  App Clima
+//
+//  Created by wendy manrique on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct App_ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
