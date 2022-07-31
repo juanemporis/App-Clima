d406a9af93ef3fdb7feb86192476bd52327b26d0
juan castañeda <juanemporis123>
2022-07-30 19:04:01 -0500

    Se diseño la vista de la app

--- App Clima/ContentView.swift
+++ App Clima/ContentView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let weatherViewModel = WeatherViewModel()
+    @StateObject var weatherViewModel = WeatherViewModel()
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Text(weatherViewModel.weatherResponseDataModel?.city ?? "No city")
+                    .foregroundColor(.white)
+                    .font(.system(size: 70))
+                Text(weatherViewModel.weatherResponseDataModel?.weather.first?.description ?? "No Descripcion")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 8 )
+                HStack {
+                    if let iconURL = weatherViewModel.weatherResponseDataModel?.weather.first?.iconURLString,
+                       let url = URL (string: "http://openweathermap.org/img/wn/\(iconURL)@2x.png") {
+                        AsyncImage(url: url) { image in
+                            image
+                        }placeholder: {
+                            ProgressView()
+                        }
+                    }
+                    Text("\(weatherViewModel.weatherResponseDataModel?.temperature.currentTemperature ?? 0.0)  °")
+                        .font(.system(size: 70))
+                        .foregroundColor(.white)
+                }
+                .padding(.top, -20)
+                HStack(spacing: 14) {
+                    Label("\(weatherViewModel.weatherResponseDataModel?.temperature.maxTemperature ?? 0.0)°" , systemImage: "thermometer.sun.fill")
+                    Label("\(weatherViewModel.weatherResponseDataModel?.temperature.minTemperature ?? 0.0)°", systemImage: "thermometer.snowflake")
+                    
+                }
+                .symbolRenderingMode(.multicolor)
+                .foregroundColor(.white)
+                Divider()
+                    .symbolRenderingMode(.multicolor)
+                    .foregroundColor(.white)
+                Divider()
+                    .foregroundColor(.white)
+                    .padding()
+                Label("\(weatherViewModel.weatherResponseDataModel?.temperature.humidity ?? 0) %",systemImage: "humidity.fill")
+                    .symbolRenderingMode(.multicolor)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .padding(.top, 32)
+            
+            .background(
+                LinearGradient(colors: [.blue, . white], startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+        }
             .task {
                 await weatherViewModel.getWeather(city: "Lima")
             }
